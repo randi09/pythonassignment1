6a1392aa72aa84f807d3e9e1636e71b710a38570
Randi Reyna <randireyna@live.com>
2021-03-10 10:26:14 -0600

    created first python program

---
+++ pythonexample.py
@@ -0,0 +1,13 @@
+# Variable
+# nameofVariable = assignedValieToVariable
+# String === "", "word", "1", "functionName"
+greeting = "Welcome to Digital Crafts Boot Camp"
+print(greeting)
+# List, sequence of values followed by [value1, value2, etc]
+#[1, 2, 3]
+# ["strings", "string2, "string3"]
+#Dictionary
+#Sequence of labeled values {key: value}
+#{"stringName": "StringValue"}
+# {"FavoriteColor": "Red"}
+# Name, assignment operator (=), value that I am assigning
