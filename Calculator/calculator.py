9d9aec7f57870f2446c7e51319c1eb9078d59ba1
Randi Reyna <randireyna@live.com>
2021-03-10 18:21:50 -0600

    first commit

---
+++ Calculator/calculator.py
@@ -0,0 +1,17 @@
+num_1 = int(input("Enter a number"))
+num_2 = int(input("Enter another number"))
+
+print(num_1 + num_2)
+
+
+num_3 = int(input("Enter a Number"))
+num_4 = int(input("Enter another Number"))
+
+print(num_3 * num_4)
+
+num_5 = int(input("Enter a Number"))
+num_6 = int(input("Enter another Number"))
+
+print(num_5 / num_6)
+
+
